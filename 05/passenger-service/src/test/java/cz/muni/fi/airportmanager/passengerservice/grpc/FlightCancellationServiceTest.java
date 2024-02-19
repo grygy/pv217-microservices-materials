d1d98b3766ac78f53f2c2386d447c8929d80c145
Adam Grygar <grygarad@gmail.com>
2024-02-19 20:27:23 +0100

    fix(grpc): fix grpc test (#12)

--- 05/passenger-service/src/test/java/cz/muni/fi/airportmanager/passengerservice/grpc/FlightCancellationServiceTest.java
+++ 05/passenger-service/src/test/java/cz/muni/fi/airportmanager/passengerservice/grpc/FlightCancellationServiceTest.java
@@ -1,5 +1,7 @@
 package cz.muni.fi.airportmanager.passengerservice.grpc;
 
+import cz.muni.fi.airportmanager.proto.FlightCancellationRequest;
+import cz.muni.fi.airportmanager.proto.FlightCancellationResponseStatus;
 import cz.muni.fi.airportmanager.proto.MutinyFlightCancellationGrpc;
 import io.quarkus.grpc.GrpcClient;
 import io.quarkus.test.junit.QuarkusTest;
@@ -7,6 +9,8 @@ import io.quarkus.test.vertx.RunOnVertxContext;
 import io.quarkus.test.vertx.UniAsserter;
 import io.smallrye.mutiny.Uni;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @QuarkusTest
 class FlightCancellationServiceTest {
@@ -17,13 +21,9 @@ class FlightCancellationServiceTest {
     @Test
     @RunOnVertxContext
     void testCancelFlight(UniAsserter asserter) {
-//        TODO this not working
-//        asserter.assertThat(() -> flightCancellationStub.cancelFlight(FlightCancellationRequest.newBuilder().setId(1).setReason("Unknown").build()),
-//                response -> {
-//                    assertEquals(FlightCancellationResponseStatus.Cancelled, response.getStatus());
-//                });
-        asserter.assertTrue(() -> Uni.createFrom().item(true));
-//        var response = flightCancellationStub.cancelFlight(FlightCancellationRequest.newBuilder().setId(1).setReason("Unknown").build()).await().indefinitely();
-//        assertEquals(FlightCancellationResponseStatus.Cancelled, response.getStatus());
+        asserter.assertThat(() -> flightCancellationStub.cancelFlight(FlightCancellationRequest.newBuilder().setId(1).setReason("Unknown").build()),
+                response -> {
+                    assertEquals(FlightCancellationResponseStatus.Cancelled, response.getStatus());
+                });
     }
 }
