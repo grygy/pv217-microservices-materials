b852eedd35d5f1641602199560a3573cc399586c
Adam Grygar <grygarad@gmail.com>
2024-04-24 21:19:55 +0200

    feat: prepare 03 assignment

--- 03-assignment/flight-service/src/main/java/cz/muni/fi/airportmanager/flightservice/service/FlightService.java
+++ 03-assignment/flight-service/src/main/java/cz/muni/fi/airportmanager/flightservice/service/FlightService.java
@@ -19,8 +19,7 @@ public class FlightService {
      */
     private final Map<Integer, Flight> flights = new HashMap<>();
 
-    @GrpcClient("passenger-service")
-    MutinyFlightCancellationGrpc.MutinyFlightCancellationStub flightCancellationStub;
+    // TODO inject flight cancellation stub using @GrpcClient
 
     /**
      * Get list of all flights
@@ -106,13 +105,5 @@ public class FlightService {
 //        TODO set flight status to CANCELLED in flights map
 //        TODO use FlightCancellationRequest.newBuilder() to create a request body
 //        TODO await the result
-        if (flights.get(id) == null) {
-            throw new IllegalArgumentException("Flight with id " + id + " does not exist");
-        }
-        flights.get(id).status = FlightStatus.CANCELLED;
-        var response = flightCancellationStub.cancelFlight(FlightCancellationRequest.newBuilder().setId(id).setReason("Unknown").build()).await().indefinitely();
-        if (response.getStatus() != FlightCancellationResponseStatus.Cancelled) {
-            throw new RuntimeException("Flight cancellation failed");
-        }
     }
 }
